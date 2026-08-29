5b1080d102d87bcd7d529da3cc7a4e5360400620
momagdy405-cmyk <momagdy405@gmail.com>
2026-08-29 22:06:17 +0300

    Q2

--- ass4/Program.cs
+++ ass4/Program.cs
@@ -13,10 +13,21 @@
             //class Book()
             //{
             //private string password = "secret";}
-        
+
             //Book b = new Book();
             //Console.WriteLine(b.password);
-            
+
+            #endregion
+            #region Q2
+            //Add an internal int copiesInStock = 5; field to Book. Print it from Main. Does it compile? Why? 
+
+
+            //class Book()
+            //{
+            //internal int copiesInStock = 5;
+            //}
+            //Book B = new Book();
+            //Console.WriteLine(B.copiesInStock); 
             #endregion
 
 
