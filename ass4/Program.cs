6a1a07c6a3fba4616667c4e0a507c6d65c53db49
momagdy405-cmyk <momagdy405@gmail.com>
2026-08-29 22:02:06 +0300

    q1

--- ass4/Program.cs
+++ ass4/Program.cs
@@ -1,15 +1,24 @@
 ﻿namespace ass4
 {
 
-    
 
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Q1
+            //Add a private string password = "secret"; field to a Book class. Try to print it from Main (outside the class). What happens, and why? 
 
+            //class Book()
+            //{
+            //private string password = "secret";}
+        
+            //Book b = new Book();
+            //Console.WriteLine(b.password);
             
+            #endregion
+
 
 
 
